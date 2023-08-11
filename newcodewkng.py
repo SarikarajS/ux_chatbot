f8377a4d534c7cbc3e8d907fe28714ac711dadd8
SarikarajS <107547206+SarikarajS@users.noreply.github.com>
2023-08-11 16:21:50 +0100

    name updated

--- newcodewkng.py
+++ newcodewkng.py
@@ -1,16 +1,17 @@
 import openai
 import gradio
 import config_data
-import logging 
+import logging
 
-logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-
+logging.basicConfig(level=logging.INFO, filename='app.log',
+                    filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+  
 openai.api_key = config_data.openapi_key
 
-messages = [{"role": "system", 
+messages = [{"role": "system",
              "content": "You are a helpful customer care assistant for an e-commerce website dealing with complaints from users. Please only answer complaint-related queries."}]
 
-def CustomChatGPT(user, name):
+def custom_chatGPT(user, name):
     messages.append({"role": "user", "content": user})
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -18,44 +19,43 @@ def CustomChatGPT(user, name):
     )
     logging.info(response, messages)
 
-    ChatGPT_reply = response.choices[0].message.content
-    messages.append({"role": "assistant", "content": ChatGPT_reply})
-    return f"{name}: {ChatGPT_reply}"  # Include the chatbot's name in the output
+    chatGPT_reply = response.choices[0].message.content
+    messages.append({"role": "assistant", "content": chatGPT_reply})
+    return f"{name}: {chatGPT_reply}"
 
-def message_and_history(input, history, name):
+
+def message_and_history(input, history):
     history = history or []
-    s = list(sum(history, ()))
-    s.append(input)
-    inp = ' '.join(s)
-    output = CustomChatGPT(inp, name)  # Pass the name to the CustomChatGPT function
+    chat_history = list(sum(history, ()))
+    chat_history.append(input)
+    input_chats = ' '.join(chat_history)
+    output = custom_chatGPT(input_chats, "Liza")
     history.append((input, output))
     return history, history
 
 
 block = gradio.Blocks(title="AI Chatbot", theme=gradio.themes.Monochrome())
 with block:
-    gradio.Markdown(
-        """
+    gradio.HTML("""
     <div style="text-align:center;">
         <h1>Customer Service Assistant</h1>
     </div>
-    """
-    )
+    """)
     with gradio.Row():
-        message = gradio.Textbox(placeholder= "Type here", label= "Let's chat")
+        message = gradio.Textbox(placeholder="type here", label="Let's chat")
         chatbot = gradio.Chatbot()
         state = gradio.State()
         message.submit(message_and_history,
-                 inputs=[message, state],
-                 outputs=[chatbot, state])
-        message.submit(lambda: '', None, message)
+                       inputs=[message, state],
+                       outputs=[chatbot, state])
+        message.submit(lambda x: gradio.update(value=''), [], [message])
 
         gradio.HTML("""
         <div style="display: flex; justify-content: center; margin-top: 20px;">
         <img src='/file=assets/images/female.jpg' style='width: 150px; height: 190px;'>
         </div>
-        <h2 style="text-align: center;">Liza</h2>  <!-- Display the chatbot's name -->
+        <h2 style="text-align: center;">Liza</h2> 
         """)
 
-    
+
 block.launch(debug=True, share=True)
